3cb0973990a792b7f5cbd061770366c8824f4e09
Lasvegasman55 <ftwalton31@yahoo.com>
2024-12-23 15:59:54 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+// In App.js
+ // In App.js
+// In App.js
+import Navbar from './components/navbar'; 
+
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar></Navbar>
+    <h1>Hello world from React</h1>
+    </div>
+  );
+}
+
+export default App;
